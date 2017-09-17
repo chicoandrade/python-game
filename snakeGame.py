fc5d8e00eae2242f0ac9b63e032ffbf4eb1e7782
Francisco Andrade <deandradefj@gmail.com>
2017-09-16 20:20:17 -0400

    Adding pause feature

--- snakeGame.py
+++ snakeGame.py
@@ -38,6 +38,7 @@ snake_position = [snake_initial_x_position, snake_initial_y_position]  # [x=hori
 snake_body = [[snake_initial_x_position, snake_initial_y_position], [snake_initial_x_position - block_size, snake_initial_y_position], [snake_initial_x_position - block_size * 2, snake_initial_y_position]]  # snake body [x, y]*3 position
 direction = 'RIGHT'
 change_to = direction
+pause = False
 
 def show_text(text="Nice try", color=color_red, position=(360, 230), duration=1):
     text_font = pygame.font.SysFont("monaco", 72)  # pygame.font.SysFont(name, size)
@@ -62,7 +63,9 @@ def game_over():
 
 
 def pop_food():
-    food_position = [(random.randrange(0, int(screen_widht/block_size))*block_size)-int(block_size/2), (random.randrange(0, int(screen_height/block_size))*block_size)-int(block_size/2)]
+    position_x = (random.randrange(int(block_size/2), int(screen_widht/block_size))*block_size)-int(block_size/2)
+    position_y = (random.randrange(int(block_size/2), int(screen_height/block_size))*block_size)-int(block_size/2)
+    food_position = [position_x, position_y]
     return food_position, True
 
 
@@ -76,6 +79,14 @@ while True:
             if event.type == pygame.QUIT:  # Checks if user requested to quit
                 quit_game()
             elif event.type == pygame.KEYDOWN:  # Checks if the user hit a key
+                if pause == True:
+                    time.sleep(1)
+                if event.key == ord('p'):
+                    if pause == False:
+                        pause = True
+                        show_text("PAUSED")
+                    else:
+                        pause = False
                 if event.key == pygame.K_UP or event.key == ord('w'):  # Check if key hitted was the up arrow key or the w button
                     change_to = 'UP'
                 if event.key == pygame.K_DOWN or event.key == ord('s'):
@@ -88,42 +99,43 @@ while True:
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
 
         # Validation of direction
-        if change_to == 'UP' and not direction == 'DOWN':
-            direction = change_to
-        if change_to == 'DOWN' and not direction == 'UP':
-            direction = change_to
-        if change_to == 'LEFT' and not direction == 'RIGHT':
-            direction = change_to
-        if change_to == 'RIGHT' and not direction == 'LEFT':
-            direction = change_to
+        if pause == False:
+            if change_to == 'UP' and not direction == 'DOWN':
+                direction = change_to
+            if change_to == 'DOWN' and not direction == 'UP':
+                direction = change_to
+            if change_to == 'LEFT' and not direction == 'RIGHT':
+                direction = change_to
+            if change_to == 'RIGHT' and not direction == 'LEFT':
+                direction = change_to
 
-        if direction == 'UP':
-            snake_position[1] = snake_position[1] - block_size  # [x = index(0) = horizontal, y = index(1) = vertical]
-        if direction == 'DOWN':
-            snake_position[1] = snake_position[1] + block_size
-        if direction == 'LEFT':
-            snake_position[0] = snake_position[0] - block_size
-        if direction == 'RIGHT':
-            snake_position[0] = snake_position[0] + block_size
+            if direction == 'UP':
+                snake_position[1] = snake_position[1] - block_size  # [x = index(0) = horizontal, y = index(1) = vertical]
+            if direction == 'DOWN':
+                snake_position[1] = snake_position[1] + block_size
+            if direction == 'LEFT':
+                snake_position[0] = snake_position[0] - block_size
+            if direction == 'RIGHT':
+                snake_position[0] = snake_position[0] + block_size
 
-        # Snake body mechanism
-        snake_body.insert(0, list(snake_position))  # adds one piece in front of the body
-        if snake_position[0] == food_position[0] - int(block_size/2) and snake_position[1] == food_position[1] - int(block_size/2):  # if the snake gets the food, we let the piece in front - she will grow
-            food_spawn = False  # theres no more food
-            food_position, food_spawn = pop_food()
-        else:
-            snake_body.pop()  # snake didnt got the food, so we will remove the bottom piece, since we added one at the front
+            # Snake body mechanism
+            snake_body.insert(0, list(snake_position))  # adds one piece in front of the body
+            if snake_position[0] == food_position[0] - int(block_size/2) and snake_position[1] == food_position[1] - int(block_size/2):  # if the snake gets the food, we let the piece in front - she will grow
+                food_spawn = False  # theres no more food
+                food_position, food_spawn = pop_food()
+            else:
+                snake_body.pop()  # snake didnt got the food, so we will remove the bottom piece, since we added one at the front
 
-        play_surface.fill(color_white)
-        for pos in snake_body:  # lets draw the snake body
-            pygame.draw.rect(play_surface, color_green, pygame.Rect(pos[0], pos[1], block_size, block_size))  # pygame.draw.rect(play surface, object color, pygame.Rect(x-pos, y-pos, x-size, y-size))
+            play_surface.fill(color_white)
+            for pos in snake_body:  # lets draw the snake body
+                pygame.draw.rect(play_surface, color_green, pygame.Rect(pos[0], pos[1], block_size, block_size))  # pygame.draw.rect(play surface, object color, pygame.Rect(x-pos, y-pos, x-size, y-size))
 
-        pygame.draw.circle(play_surface, color_brown, (food_position), int(block_size/2))  # surface, color, position, radius, width
+            pygame.draw.circle(play_surface, color_brown, (food_position), int(block_size/2))  # surface, color, position, radius, width
 
-        if snake_position[0] > screen_widht or snake_position[0] < 0:  # if snakes goes outside the screen
-            game_over()
-        if snake_position[1] > screen_height or snake_position[1] < 0:
-            game_over()
+            if snake_position[0] > screen_widht or snake_position[0] < 0:  # if snakes goes outside the screen
+                game_over()
+            if snake_position[1] > screen_height or snake_position[1] < 0:
+                game_over()
 
         pygame.display.flip() # update the frame
         fps_controller.tick(framerate) # controls the framerate
